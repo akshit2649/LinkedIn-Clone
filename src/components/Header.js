0f15d2e92aa5041d3bf4c73d1f99df1818aef26a
Akshit Thakur <akshit2649@gmail.com>
2022-07-31 20:51:13 +0530

    Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import Me from "../asset/DSC_0328.jpg";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+          alt=""
+        />
+
+        <form className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </form>
+      </div>
+
+      <div className="header__right">
+        <HeaderOption title="Home" Icon={HomeIcon} />
+        <HeaderOption title="My Network" Icon={SupervisorAccountIcon} />
+        <HeaderOption title="Jobs" Icon={BusinessCenterIcon} />
+        <HeaderOption title="Messaging" Icon={ChatIcon} />
+        <HeaderOption title="Notifications" Icon={NotificationsIcon} />
+        <HeaderOption title="me" avatar={Me} />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
